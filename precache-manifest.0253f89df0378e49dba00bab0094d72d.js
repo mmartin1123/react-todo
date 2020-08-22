6a1d69eeb437aed5fcf11a3987322473480b23e3
Michael Martin <mictin3@gmail.com>
2020-08-22 09:12:20 -0600

    Updates

---
+++ precache-manifest.0253f89df0378e49dba00bab0094d72d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3481848e761dd482109b4a65bed64fe",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1a053b78b8abed792e93",
+    "url": "/react-todo/static/css/main.732da882.chunk.css"
+  },
+  {
+    "revision": "bf29516a15781b42df8b",
+    "url": "/react-todo/static/js/2.857b4de0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.857b4de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a053b78b8abed792e93",
+    "url": "/react-todo/static/js/main.72b0ed92.chunk.js"
+  },
+  {
+    "revision": "1c15613056b13a1bce12",
+    "url": "/react-todo/static/js/runtime-main.812322bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
